7e3915a796e964ecc0e9259e27edda7b48f35817
MeetRamatri <meetramatri12@gmail.com>
2026-02-26 11:45:20 +0530

    Add Command

--- index.ts
+++ index.ts
@@ -2,8 +2,16 @@
 const {Command}= require('commander');
 const program = new Command();
 program
-.command('greet <name>')
-.action((name: string) => {
-    console.log(`Hello, ${name}! Welcome to my CLI tool.`);
-});
+    .command('greet <name>')
+    .action((name: string) => {
+        console.log(`Hello, ${name}! Welcome to my CLI tool.`);
+    });
+
+program
+    .command('add <num1> <num2>')
+    .action((num1: string, num2: string) => {
+        const sum = parseFloat(num1) + parseFloat(num2);
+        console.log(`The sum of ${num1} and ${num2} is ${sum}.`);
+    });
+    
 program.parse();
